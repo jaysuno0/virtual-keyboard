d664ae6cc21d5d247bbc26fa4a71ebb60df79708
Daniil Kochkin <jaysun0@yandex.ru>
2022-05-09 09:53:45 +0300

    feat: connect virtual keyboard with user keyboard

--- virtual-keyboard/src/components/keyboard/keyboard.js
+++ virtual-keyboard/src/components/keyboard/keyboard.js
@@ -2,6 +2,7 @@ import { createElement } from '../../assets/js/helpers';
 import chars from '../../assets/js/chars';
 
 const keyboard = createElement('div', ['main__keyboard', 'keyboard']);
+const buttons = {};
 
 function createButton(btn) {
   const button = createElement('div', ['btn', 'keyboard__btn']);
@@ -14,9 +15,38 @@ function createButton(btn) {
     button.append(alt);
   }
 
+  if (btn.code.includes('lang')
+      || btn.code.includes('Backspace')
+      || btn.code.includes('Enter')
+      || btn.code.includes('Shift')
+      || btn.code.includes('Alt')
+      || btn.code.includes('Control')
+      || btn.code.includes('Meta')
+      || btn.code.includes('Arrow')
+      || btn.code.includes('Arrow')
+      || btn.code.includes('Lock')
+      || btn.code.includes('Lock')
+      || btn.code.includes('Tab')
+      || btn.code.includes('Del')
+  ) button.classList.add('functional');
+
   keyboard.append(button);
+
+  buttons[btn.code] = {
+    char: btn.char,
+    alt: btn.alt,
+    btn: button,
+  };
 }
 
-chars.en.forEach((char) => createButton(char));
+chars.ru.forEach((char) => createButton(char));
+
+document.addEventListener('keydown', (event) => {
+  buttons[event.code].btn.classList.add('pressed');
+});
+
+document.addEventListener('keyup', (event) => {
+  buttons[event.code].btn.classList.remove('pressed');
+});
 
 export default keyboard;
